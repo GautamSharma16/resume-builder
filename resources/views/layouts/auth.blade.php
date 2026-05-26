00863332b3d371b14cd2324e213b8e829e0b08ea
Gautam Sharma <155160574+GautamSharma16@users.noreply.github.com>
2026-05-26 12:49:53 +0530

    commit -m "Add favicon to all layouts and welcome page"

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') | {{ config('app.name', 'Resume Builder') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
